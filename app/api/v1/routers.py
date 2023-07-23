bed5491b6fdc680995ccd0c14f3d79e5495bfaa9
Artem Nosov <nosov1995@gmail.com>
2023-07-23 19:00:11 +0200

    init auto filling base

--- app/api/v1/routers.py
+++ app/api/v1/routers.py
@@ -2,8 +2,12 @@ from fastapi import APIRouter
 
 from app.api.v1.subscription import subscription_router
 from app.api.v1.source import source_router
+from app.api.v1.auto import auto_router
 
 api_router_v1 = APIRouter(prefix='/api/v1')
+api_router_v1.include_router(
+    auto_router, prefix='/auto', tags=['Auto']
+)
 api_router_v1.include_router(
     source_router, prefix='/source', tags=['Sources']
 )
